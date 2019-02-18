450df61f49b6f2f92a76b929c320096ba7da803b
Mark <mn16660@my.bristol.ac.uk>
2019-02-18 10:25:59 +0000

    Cornell Box Appearing, But Unwanted Lines Show Up

--- raytracer/Source/skeleton.cpp
+++ raytracer/Source/skeleton.cpp
@@ -19,11 +19,13 @@ SDL_Event event;
 #define FULLSCREEN_MODE false
 
 
+
 /* ----------------------------------------------------------------------------*/
 /* VARIABLES                                                                   */
 /* ----------------------------------------------------------------------------*/
 
-float focalLength = SCREEN_HEIGHT;
+float focalLength = 128;
+vec4 cameraPos( 0, 0.0, -2.0, 1.0);
 
 /* ----------------------------------------------------------------------------*/
 /* STRUCTS                                                                   */
@@ -72,34 +74,38 @@ void Draw(screen* screen) {
   memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
 
   vec3 colour(0.0,0.0,0.0);
-  for(int i=0; i<1000; i++)
-    {
-      uint32_t x = rand() % screen->width;
-      uint32_t y = rand() % screen->height;
-      PutPixelSDL(screen, x, y, colour);
-    }
+  // for(int i=0; i<1000; i++)
+  //   {
+  //     uint32_t x = rand() % screen->width;
+  //     uint32_t y = rand() % screen->height;
+  //     PutPixelSDL(screen, x, y, colour);
+  //   }
 
   // Raytrace begin
   vector<Triangle> triangles;
   LoadTestModel( triangles );
-  vec4 cameraPos( 0, 0, focalLength, 1.0);
 
-  for (int v = -SCREEN_HEIGHT/2; v < SCREEN_HEIGHT/2; v++) {
-    for (int u = -SCREEN_WIDTH/2; u < SCREEN_WIDTH/2; u++) {
-      vec4 dir = vec4(u - (SCREEN_WIDTH/2), v - (SCREEN_HEIGHT/2), focalLength, 1.0);
+  /*RESCALE THE IMAGE */
+  for (int i = 0; i < triangles.size(); i++) {
+      triangles[i].v0 *= vec4(1, 1, 1, 1.0); 
+      triangles[i].v1 *= vec4(1, 1, 1, 1.0); 
+      triangles[i].v2 *= vec4(1, 1, 1, 1.0); 
+  }
+
+  // u and v are coordinates on the 2D screen
+  for (int v = 0; v < SCREEN_HEIGHT; v++) {
+    for (int u = 0; u < SCREEN_WIDTH; u++) {
+      vec4 dir = vec4(u - SCREEN_WIDTH/2, v - SCREEN_HEIGHT/2, focalLength, 1.0);
 
       // vec4 normDir = 1/(sqrt(dir * dir)) * dir;
       Intersection closestIntersection;
 
-      // std::cout << "here" << '\n';
-
       if (ClosestIntersection(cameraPos, dir, triangles, closestIntersection) == true) {
-        // std::cout << "Intersection found, distance = " + std::to_string(closestIntersection.distance) + "\n";
-        PutPixelSDL(screen, u + SCREEN_WIDTH/2, v + SCREEN_HEIGHT/2, triangles[closestIntersection.triangleIndex].color);
+        //std::cout << "Intersection found, distance = " + std::to_string(closestIntersection.distance) + "\n";
+        PutPixelSDL(screen, u, v, triangles[closestIntersection.triangleIndex].color);
       }
       else {
-        // std::cout << "No intersection: paint black\n";
-        PutPixelSDL(screen, u + SCREEN_WIDTH/2, v + SCREEN_HEIGHT/2, colour);
+        PutPixelSDL(screen, u, v, colour);
       }
 
       // std::cout << std::to_string(x) + ", " + std::to_string(y) + " printed " << '\n';
@@ -115,36 +121,42 @@ bool Update() {
   float dt = float(t2-t);
   t = t2;
 
+  cout << "Render time : " << dt << " ms." <<endl;
+  cout << to_string(cameraPos[0]) + " " + to_string(cameraPos[1]) + " " + to_string(cameraPos[2]) + " " << endl;
+
   SDL_Event e;
   while(SDL_PollEvent(&e))
     {
       if (e.type == SDL_QUIT)
-	{
-	  return false;
-	}
-      else
-	if (e.type == SDL_KEYDOWN)
-	  {
-	    int key_code = e.key.keysym.sym;
-	    switch(key_code)
+	    {
+	      return false;
+	    }
+      else if (e.type == SDL_KEYDOWN)
+	    {
+	      int key_code = e.key.keysym.sym;
+	      switch(key_code)
 	      {
-	      case SDLK_UP:
-		/* Move camera forward */
-		break;
-	      case SDLK_DOWN:
-		/* Move camera backwards */
-		break;
-	      case SDLK_LEFT:
-		/* Move camera left */
-		break;
-	      case SDLK_RIGHT:
-		/* Move camera right */
-		break;
-	      case SDLK_ESCAPE:
-		/* Move camera quit */
-		return false;
+	        case SDLK_UP:
+		        cameraPos += vec4(0, 0, 0.5, 0);
+            cout << "KEYPRESS." <<endl;
+		      break;
+	        case SDLK_DOWN:
+		        cameraPos += vec4(0, 0, -0.5, 0);
+            cout << "KEYPRESS." <<endl;
+		      break;
+	        case SDLK_LEFT:
+		        cameraPos += vec4(0.5, 0, 0, 0);
+            cout << "KEYPRESS." <<endl;
+		      break;
+	        case SDLK_RIGHT:
+		        cameraPos += vec4(-0.5, 0, 0, 0);
+            cout << "KEYPRESS." <<endl;
+		      break;
+	        case SDLK_ESCAPE:
+		      /* Move camera quit */
+		      return false;
 	      }
-	  }
+	    }
     }
   return true;
 }
@@ -155,18 +167,19 @@ bool ClosestIntersection( vec4 start, vec4 dir,
 
     // Don't check for intersection for large t
     float bound = std::numeric_limits<float>::max();
-    float minDist = INFINITY;
 
     closestIntersection.distance = bound;
 
-    for (size_t i = 0; i < triangles.size(); i++) {
+    for (int i = 0; i < triangles.size(); i++) {
       vec4 v0 = triangles[i].v0;
       vec4 v1 = triangles[i].v1;
       vec4 v2 = triangles[i].v2;
 
-      vec3 e1 = vec3(v1.x-v0.x,v1.y-v0.y,v1.z-v0.z);
-      vec3 e2 = vec3(v2.x-v0.x,v2.y-v0.y,v2.z-v0.z);
-      vec3 b = vec3(start.x-v0.x,start.y-v0.y,start.z-v0.z);
+      vec3 e1 = vec3(v1.x - v0.x ,v1.y - v0.y,v1.z - v0.z);
+      vec3 e2 = vec3(v2.x - v0.x ,v2.y - v0.y,v2.z - v0.z);
+      vec3 b = vec3(start.x-v0.x, start.y-v0.y, start.z-v0.z);
+
+      //std::cout << to_string(b[0]) + " " + to_string(b[1]) + " " + to_string(b[2]) << '\n';
 
       // bottleneck
       // glm::mat3 A;
@@ -184,18 +197,20 @@ bool ClosestIntersection( vec4 start, vec4 dir,
       bool check5 = x[0] < bound;
       bool check6 = x[0] < closestIntersection.distance;
 
-      // std::cout << to_string(check1) + to_string(check2) + to_string(check3) + to_string(check4) + to_string(check5) + to_string(check6) << '\n';
-      // Check against inequalities, if true intersection occurred
       if ( check1 && check2 && check3 && check4 && check5 && check6 ) {
-        if (closestIntersection.distance < minDist) {
-          closestIntersection.position = x4;
-          closestIntersection.distance = x[0];
-          closestIntersection.triangleIndex = i;
-          // std::cout << "accept" << '\n';
-        }
+        closestIntersection.position = x4;
+        closestIntersection.distance = x[0];
+        closestIntersection.triangleIndex = i;
       }
     }
 
-    if (closestIntersection.distance != INFINITY) return true;
-    else return false;
-}
+    if ( closestIntersection.distance < bound ) {
+        return true;
+    }
+
+    else 
+    {
+      return false;
+    }
+  }
+
