4604f0de5c9d11b859f147d1a4f20489599848c7
Faizaan <ss16161@my.bristol.ac.uk>
2019-03-14 17:11:31 +0000

    Points drawn

--- rasteriser/Source/skeleton.cpp
+++ rasteriser/Source/skeleton.cpp
@@ -17,15 +17,28 @@ SDL_Event event;
 #define SCREEN_HEIGHT 256
 #define FULLSCREEN_MODE false
 
+
+/* ----------------------------------------------------------------------------*/
+/* VARIABLES                                                                   */
+/* ----------------------------------------------------------------------------*/
+
+float focalLength = 256;
+vec4 cameraPos( 0, 0, -3.001, 1 );
+// float yaw = 0.0;
+// mat4 R(1.0f);
+
+
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
+/* ----------------------------------------------------------------------------*/
 
 bool Update();
 void Draw(screen* screen);
+void VertexShader( const vec4& v, glm::ivec2& p );
 
 int main( int argc, char* argv[] )
 {
-  
+
   screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
 
   while ( Update())
@@ -43,16 +56,25 @@ int main( int argc, char* argv[] )
 /*Place your drawing here*/
 void Draw(screen* screen)
 {
+  vector<Triangle> triangles;
+  LoadTestModel( triangles );
+
   /* Clear buffer */
   memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-  
-  vec3 colour(1.0,0.0,0.0);
-  for(int i=0; i<1000; i++)
-    {
-      uint32_t x = rand() % screen->width;
-      uint32_t y = rand() % screen->height;
-      PutPixelSDL(screen, x, y, colour);
+  for( uint32_t i=0; i<triangles.size(); ++i ) {
+     vector<vec4> vertices(3);
+
+     vertices[0] = triangles[i].v0;
+     vertices[1] = triangles[i].v1;
+     vertices[2] = triangles[i].v2;
+
+     for(int v=0; v<3; ++v) {
+       glm::ivec2 projPos;
+       VertexShader( vertices[v], projPos );
+       vec3 color(1,1,1);
+       PutPixelSDL( screen, projPos.x, projPos.y, color );
     }
+  }
 }
 
 /*Place updates of parameters here*/
@@ -93,7 +115,25 @@ bool Update()
 		/* Move camera quit */
 		return false;
 	      }
-	  }  
+	  }
     }
   return true;
+}
+
+void VertexShader( const vec4& v, glm::ivec2& p ) {
+  float x3D = v[0] - cameraPos[0];
+  float y3D = v[1] - cameraPos[1];
+  float z3D = v[2] - cameraPos[2];
+
+
+  float x = (focalLength * (x3D / z3D)) + (SCREEN_WIDTH/2);
+  float y = (focalLength * (y3D / z3D)) + (SCREEN_HEIGHT/2);
+
+  int x2D = static_cast<int>(x);
+  int y2D = static_cast<int>(y);
+
+  p[0] = x2D;
+  p[1] = y2D;
+
+  std::cout << x2D << " and " << y2D << '\n';
 }
